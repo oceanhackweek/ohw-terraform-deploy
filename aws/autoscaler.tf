33da047d5143d82e85d80512108e0de818115532
Sebastian Alvis <sebialvis@gmail.com>
2020-07-29 10:56:38 -0700

    Move Helm provider to a file that has a Helm release

--- aws/autoscaler.tf
+++ aws/autoscaler.tf
@@ -2,6 +2,15 @@ terraform {
   required_version = ">= 0.12.6"
 }
 
+provider "helm" {
+  version = "~> 1.1"
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 # Create IAM role + automatically make it available to cluster autoscaler service account
 module "iam_assumable_role_admin" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
--- aws/main.tf
+++ aws/main.tf
@@ -199,13 +199,3 @@ module "eks" {
   map_users    = var.map_users
   map_accounts = var.map_accounts
 }
-
-
-provider "helm" {
-  version = "~> 1.1"
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
